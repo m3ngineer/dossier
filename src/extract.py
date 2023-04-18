57ada501ec1cdc9e2a631e813fa98b44c66b4f2b
Matthew Eng <matthew.eng2@gmail.com>
2023-04-18 08:59:58 -0400

    Add skeleton for extracting data from other sources

---
+++ src/extract.py
@@ -0,0 +1,7 @@
+# Extract the source data from public provider databases
+# - OpenPayments
+# - Pubmed
+# - Clinical Trials
+
+# Add data to database
+# At time of run, extracts data and creates a biography
